566bb8bda3926bd0b4a289776516de5c9785cfdf
tangyiwen <tangyiwen@cissdata.com>
2018-06-13 16:08:05 +0800

    basic

---
+++ src/components/header/head.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id='head-top'>
+        <p>这是头部</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'headTop',
+  data () {
+    return {
+      msg: 'tangyiwen'
+    }
+  }
+}
+</script>
---
+++ src/page/home/home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <div>
+        <p>这是图片部分</p>
+    </div>
+    <div>
+        <p>这是文字部分</p>
+    </div>
+    <div>
+        <p>这是底部</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import head from '../../components/header/head'
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'tangyiwen'
+    }
+  },
+  components: {
+      'head-top' : head
+  }
+}
+</script>
