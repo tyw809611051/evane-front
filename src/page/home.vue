a2791ea5e1b26c6ac6998dac5ea67cece340c3fd
Administrator <tangyiwen@cissdata.com>
2018-12-10 18:45:49 +0800

    init

--- src/page/home.vue
+++ src/page/home.vue
@@ -1,72 +1,16 @@
 <template lang="html">
 <div class="home-container">
-  <el-row class="container">
-    <el-col :span="24" class="header">
-            <el-col :span="10" class="logo" :class="collapsed?'logo-collapse-width':'logo-width'">{{collapsed?'':sysName}}</el-col>
-            <el-col :span="10">
-        <div class="tools" @click.prevent="collapseFun">
-          <i class="fa fa-align-justify"></i>
-                </div>
-            </el-col>
-            <el-col :span="4" class="userinfo">
-                <el-dropdown trigger="hover">
-                    <span class="el-dropdown-link userinfo-inner">{{sysUserName}}</span>
-                    <el-dropdown-menu slot="dropdown">
-                        <el-dropdown-item>我的消息</el-dropdown-item>
-                        <el-dropdown-item>设置</el-dropdown-item>
-                        <el-dropdown-item divided @click.native="logoutFun">退出登录</el-dropdown-item>
-                    </el-dropdown-menu>
-                </el-dropdown>
-            </el-col>
-        </el-col>
-    <el-col :span="24" class="main">
-      <aside :class="collapsed?'menu-collapsed':'menu-expanded'">
-        <!--导航菜单-->
-       <el-menu :default-active="$route.path" class="el-menu-vertical-demo" unique-opened router v-show="!collapsed">
-         <template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">
-           <el-submenu :index="index+''" v-if="!item.leaf">
-             <template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>
-             <el-menu-item v-for="child in item.children" :index="child.path" :key="child.path" v-if="!child.hidden">{{child.name}}</el-menu-item>
-           </el-submenu>
-           <el-menu-item v-if="item.leaf&&item.children.length>0" :index="item.children[0].path"><i :class="item.iconCls"></i>{{item.children[0].name}}</el-menu-item>
-         </template>
-       </el-menu>
-       <!--导航菜单-折叠后-->
-       <ul class="el-menu el-menu-vertical-demo collapsed" v-show="collapsed" ref="menuCollapsed">
-         <li v-for="(item,index) in $router.options.routes" v-if="!item.hidden" class="el-submenu item">
-           <template v-if="!item.leaf">
-             <div class="el-submenu__title" style="padding-left: 20px;" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)">
-               <i :class="item.iconCls"></i>
-             </div>
-             <ul class="el-menu submenu" :class="'submenu-hook-'+index" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)">
-               <li v-for="child in item.children" v-if="!child.hidden" :key="child.path" class="el-menu-item" style="padding-left: 40px;height: 56px;line-height: 56px;" :class="$route.path==child.path?'is-active':''" @click="$router.push(child.path)">{{child.name}}</li>
-             </ul>
-           </template>
-           <template v-else>
-             <li class="el-submenu">
-               <div class="el-submenu__title el-menu-item" style="padding-left: 20px;height: 56px;line-height: 56px;padding: 0 20px;" :class="$route.path==item.children[0].path?'is-active':''" @click="$router.push(item.children[0].path)"><i :class="item.iconCls"></i></div>
-             </li>
-           </template>
-         </li>
-       </ul>
-     </aside>
-     <section class="content-container">
-       <div class="grid-content bg-purple-light">
-         <el-col :span="24" class="breadcrumb-container">
-           <strong class="title">{{$route.name}}</strong>
-           <el-breadcrumb separator="/" class="breadcrumb-inner">
-             <el-breadcrumb-item v-for="item in $route.matched" :key="item.path">{{ item.name }}</el-breadcrumb-item>
-           </el-breadcrumb>
-         </el-col>
-         <el-col :span="24" class="content-wrapper">
-           <transition name="fade" mode="out-in">
-             <router-view></router-view>
-           </transition>
-         </el-col>
-       </div>
-     </section>
-   </el-col>
- </el-row>
+    <el-container>
+        <el-header >Header</el-header>
+        <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-container>
+                <el-main>Main</el-main>
+                <el-footer>Footer</el-footer>
+            </el-container>
+        </el-container>
+    </el-container>
+    
 </div>
 </template>
 
@@ -75,7 +19,7 @@ export default {
     components: {},
     data() {
         return {
-            sysName: 'VueDemo',
+            sysName: '管理后台',
             sysUserName: '',
             collapsed: false
         }
@@ -114,102 +58,48 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.container {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    width: 100%;
-    .header {
-        height: 60px;
-        line-height: 60px;
-        background: #bbdbfb;
-        color: #fff;
-        .userinfo {
-            text-align: right;
-            padding-right: 35px;
-            float: right;
-            .userinfo-inner {
-                cursor: pointer;
-                color: #fff;
-            }
-        }
-        .logo {
-            height: 60px;
-            font-size: 22px;
-            padding-left: 20px;
-            padding-right: 20px;
-            border-color: rgba(238,241,146,0.3);
-            border-right-width: 1px;
-            border-right-style: solid;
-        }
-        .logo-width {
-            width: 230px;
-        }
-        .logo-collapse-width {
-            width: 60px;
-        }
-        .tools {
-            padding: 0 23px;
-            width: 14px;
-            height: 60px;
-            line-height: 60px;
-            cursor: pointer;
-        }
-    }
-    .main {
-        display: flex;
-        position: absolute;
-        top: 60px;
-        bottom: 0;
-        overflow: hidden;
-        aside {
-            flex: 0 0 230px;
-            width: 230px;
-            .el-menu {
-                height: 100%;
-            }
-            .collapsed {
-                width: 60px;
-                .item {
-                    position: relative;
-                }
-                .submenu {
-                    position: absolute;
-                    top: 0;
-                    left: 60px;
-                    z-index: 99999;
-                    height: auto;
-                    display: none;
-                }
-            }
-        }
-        .menu-collapsed {
-            flex: 0 0 60px;
-            width: 60px;
-        }
-        .menu-expanded {
-            flex: 0 0 230px;
-            width: 230px;
-        }
-        .content-container {
-            flex: 1;
-            overflow-y: scroll;
-            padding: 20px;
-            .breadcrumb-container {
-                .title {
-                    width: 200px;
-                    float: left;
-                    color: #475669;
-                }
-                .breadcrumb-inner {
-                    float: right;
-                }
-            }
-            .content-wrapper {
-                background-color: #fff;
-                box-sizing: border-box;
-            }
-        }
-    }
-}
+  .home-container {
+      height: 100%;
+      margin: 0 auto;
+      width: 100%;
+  }
+  .el-container {
+      height: 100%;
+  }
+  .el-header, .el-footer {
+    color: #000000;
+    background-color: rgb(161, 255, 255);
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    background-color: rgb(234, 238, 238);
+    text-align: center;
+    line-height: 200px;
+    width: 50%;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: gray;
+    background-color: #ffffff;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Login from '@/page/login.vue'
 import home from '@/page/home.vue'
 import NotFound from '@/page/404.vue'
 import MenuTable from '@/page/menu1/menuTab.vue'
+import MenuTable1 from '@/page/menu2/menuTab.vue'
 
 Vue.use(Router)
 
@@ -17,16 +18,24 @@ export default new Router({
       name: '导航一',
       component: home,
       iconCls: 'el-icon-message',
-      children: [{
-        path: '/menutab',
-        component: MenuTable,
-        name: 'Tab'
-      }]
+      children: [
+        {
+          path: '/menutab',
+          component: MenuTable,
+          name: 'Tab'
+        },
+        {
+          path: '/menutab1',
+          component: MenuTable1,
+          name: 'Tab1'
+        }
+     ]
     },
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: Login,
+      hidden: true
     },
     {
       path: '/404',
